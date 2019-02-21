208f78a0dc2487ac02f682520f82648d224e28ee
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-02-21 15:43:45 -0300

    Forms classes and html

--- flask_blog.py
+++ flask_blog.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, url_for
+
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ce88b6999020eb604e7cdb913cf925e1324812ff'
 
 posts = [
     {
@@ -26,6 +31,18 @@ def index():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='login', form=form)
 
 
 if __name__ == '__main__':
