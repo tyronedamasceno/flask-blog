670a8a8ec5bb1fb8fc31ab48797b63895fd3bc32
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-02-21 14:36:10 -0300

    Basic templates with dummy data, receiving parameters

--- flask_blog.py
+++ flask_blog.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def index():
-    return "<h1>Home page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return render_template('about.html', title='about')
