3519285719d324ea0458fde16103e1ce870ab183
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-02-21 15:11:52 -0300

    Add navbar and css

--- flask_blog.py
+++ flask_blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
@@ -25,4 +25,8 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='about')
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
